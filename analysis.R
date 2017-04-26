e469407ba5014e26eef7dc0353ce8da7a4e7f814
Alejandro Garcia <agarc579@fiu.edu>
2017-04-26 19:40:50 -0400

    Calculated the median of the age column, excluding the NA values. Populated those NA values with that median result.

--- analysis.R
+++ analysis.R
@@ -6,3 +6,7 @@ titanic_dataset <- read_excel("~/Downloads/titanic3.xls", na = "")
 titanic_df <- data.frame(titanic_dataset)
 
 titanic_df$embarked[is.na(titanic_df$embarked)] <- "S"
+
+mean_value_of_age_without_na <- mean(titanic_df$age, na.rm = TRUE)
+
+titanic_df$age[is.na(titanic_df$age)] <- mean_value_of_age_without_na
