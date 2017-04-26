491be472e9dbe7d48daf6ae476c474a23545182e
Alejandro Garcia <agarc579@fiu.edu>
2017-04-26 19:32:25 -0400

    replaced all NA values from the embarked column to "S" for Southampton.

--- analysis.R
+++ analysis.R
@@ -1,1 +1,8 @@
 #initial commit
+
+titanic_dataset <- read_excel("~/Downloads/titanic3.xls", na = "")
+#embarked column has some missing values, better to change to NA values
+
+titanic_df <- data.frame(titanic_dataset)
+
+titanic_df$embarked[is.na(titanic_df$embarked)] <- "S"
