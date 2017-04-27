a03f59c6530023aca5e797ebc5f1c68aa67b4ada
Alejandro Garcia <agarc579@fiu.edu>
2017-04-26 21:08:05 -0400

    Created a has_cabin_number column and populated it depending on (1) if it has a cabin or (0) if it does not.

--- analysis.R
+++ analysis.R
@@ -10,3 +10,12 @@ titanic_df$embarked[is.na(titanic_df$embarked)] <- "S"
 mean_value_of_age_without_na <- mean(titanic_df$age, na.rm = TRUE)
 
 titanic_df$age[is.na(titanic_df$age)] <- mean_value_of_age_without_na
+
+titanic_df$boat[is.na(titanic_df$boat)] <- "None"
+
+titanic_df["has_cabin_number"] <- NA
+
+
+titanic_df$has_cabin_number[which(is.na(titanic_df$cabin) == TRUE)] <- "0"
+titanic_df$has_cabin_number[which(is.na(titanic_df$cabin) == FALSE)] <- "1"
+
